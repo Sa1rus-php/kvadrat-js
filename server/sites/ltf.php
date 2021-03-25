24d58efef0c278a593d6b806595026d713c116bb
Dalgoaaa <drekhlitskii@gmail.com>
2021-03-25 17:04:06 +0200

    ооп начало

--- server/sites/ltf.php
+++
@@ -1,3 +0,0 @@
-<?php
-include 'func.php';
-echo Txt($_GET['file']);
--- server/sites/users.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'func.php';
-echo '<pre>';
-set_include_path('../download');
-include $_GET['file'];
-
-echo '</pre>';
