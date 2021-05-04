5987e818ea5c38b34c20af4095580e22c58c763e
Dalgoaaa <drekhlitskii@gmail.com>
2021-05-05 00:18:20 +0300

    aunt

---
+++ server/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` int(99) NOT NULL,
+  `full_name` varchar(355) DEFAULT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(500) DEFAULT NULL,
+  `avatar` varchar(500) DEFAULT NULL
+)
---
+++ server/index.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+if ($_SESSION['user']) {
+    header('Location: profile.php');
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <title>Rekhlitskiy</title>
+    <link rel="stylesheet" href="set/main.css">
+</head>
+<body>
+
+    <form action="aunt/signin.php" method="post">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Логин">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Пароль">
+        <button type="submit">Войти</button>
+        <p>
+            У вас нет аккаунта? - <a href="/register.php">Зарегистрируйтесь</a>!
+        </p>
+        <?php
+            if ($_SESSION['message']) {
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }
+            unset($_SESSION['message']);
+        ?>
+    </form>
+
+</body>
+</html>
