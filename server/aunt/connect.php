981ecb910a81364e5176a1f4579e43bf85352a40
Dalgoaaa <drekhlitskii@gmail.com>
2021-05-10 00:25:33 +0300

    test

---
+++ server/aunt/connect.php
@@ -0,0 +1,6 @@
+<?php
+        $dsn = 'mysql:dbname=kyrsi;host=kyrs_mysql';
+        $user = 'root';
+        $password = 'bfigf_iee-bifjb';
+        $connect = new PDO($dsn, $user, $password);
+
---
+++ server/register.php
@@ -0,0 +1,47 @@
+<?php
+    session_start();
+    if ($_SESSION['user']) {
+        header('Location: profile.php');
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rekhlitskiy</title>
+    <link rel="stylesheet" href="set/css.css">
+</head>
+<body>
+    <form action="aunt/signup.php" method="post" enctype="multipart/form-data">
+        <label>ФИО</label>
+        <input type="text" name="full_name" placeholder="Введите свое ФИО">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Почта</label>
+        <input type="email" name="email" placeholder="Введите адрес своей почты">
+        <label>Серия паспорта</label>
+        <input type="text" name="series" placeholder="Введите серию паспорта">
+        <label>Номер паспорта</label>
+        <input type="text" name="num" placeholder="Введите номер паспорта">
+        <label>Дата выдачи</label>
+        <input type="text" name="date" placeholder="Введите дату выдачи">
+        <label>Изображение профиля</label>
+        <input type="file" name="avatar">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <label>Подтверждение пароля</label>
+        <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
+        <button type="submit">Войти</button>
+        <p>
+            У вас уже есть аккаунт? - <a href="/">Авторизируйтесь</a>!
+        </p>
+        <?php
+            if ($_SESSION['message']) {
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }
+            unset($_SESSION['message']);
+        ?>
+    </form>
+</body>
+</html>
