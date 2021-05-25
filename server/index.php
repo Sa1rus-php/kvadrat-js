e0675e502f55ea26e291ccd34b78f0639571a531
Dalgoaaa <drekhlitskii@gmail.com>
2021-05-25 08:29:38 +0300

    kvadrat

--- server/index.php
+++ server/index.php
@@ -1,22 +1,28 @@
-<!doctype html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-            crossorigin="anonymous"></script>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>test</title>
+    <meta charset="utf-8">
+
+    <style>
+        #kvadrat{
+            width: 100px;
+            height: 100px;
+            background: #000000;
+            position: absolute;
+        }
+    </style>
+<script type="text/javascript">
+
+    document.addEventListener('mousemove', e => {
+        kvadrat.style.left = e.pageX + "px";
+        kvadrat.style.top = e.pageY + "px";
+    });
+    var w = document.documentElement.clientWidth;
+    var h = document.documentElement.clientHeight;
+
+</script>
 </head>
 <body>
-<div class="d-flex justify-content-center">
-<a href="./aunt/aunt_form.php"><button type="button" class="btn btn-outline-primary">Sign in</button></a>
-<pre> </pre>
-<a href="./reg/reg_form.php"><button type="button" class="btn btn-outline-primary">Sign up</button></a>
-</div>
+<div id="kvadrat"></div>
 </body>
 </html>
